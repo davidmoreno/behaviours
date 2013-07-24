dc25de212c313e044b115693a074b238f6d90d0c
Víctor Guzmán <fawques@gmail.com>
2013-07-24 10:45:40 +0200

    limpieza de botones.js

--- plugins/Interfaz/static/js/modules/botones.js
+++ plugins/Interfaz/static/js/modules/botones.js
@@ -3,6 +3,10 @@
 
 (function()
 {
+function clickButton(id){
+  main.lua_exec('manager.notify('+id+')')
+}
+
 var showInterface = function(){
   // Esto es como llamar a this.configureDialogSetup pero sin showDefaultButtons, con lo que no asigna el acceptConfig al close
   main.showDialog()
@@ -16,51 +20,18 @@ var showInterface = function(){
   })
   tt.append(name).append('</span> <div class="type">('+this.type+')</div>')
 
-  var tr=$('<tr>')
-  var table=$('<table>').append(tr)
-
-  
-
-    
-//   if (this.params.exec)
-//     code.val(this.params.exec)
-//   if (this.params.check)
-//     code.val(this.params.check)
-    
-  var cheat_sheet=$('<ul>')
 	var state=main.behaviour.state
-  var b= $('<div class="buttonsPanel">').css({'margin-left': 'auto','margin-right': 'auto','width':'200px'})
-
-    b.append("<button onclick='clickButton(\"" + this.params['arriba'] + "\")' style='margin-left:30px' id= 'Up'>Up</button>").append("<br/>").append("<button onclick='clickButton(\"" + this.params['izquierda'] + "\")' id='Left'>Left</button>").append("<button onclick='clickButton(\"" + this.params['derecha'] + "\")' id='Right'>Right</button>").append("<br/>").append("<button onclick='clickButton(\"" + this.params['abajo'] + "\")' style='margin-left:20px'  id='Down'>Down</button>")
+  var buttonPanel= $('<div class="buttonsPanel">').css({'margin-left': 'auto','margin-right': 'auto','width':'200px'})
+  var buttonUp = $('<button>').css({'margin-left':'30px'}).click(function(){clickButton(that.params['arriba'])}).attr('id','Up').html('Up');
+  var buttonDown = $('<button>').css({'margin-left':'30px'}).click(function(){clickButton(that.params['abajo'])}).attr('id','Down').html('Down');
+  var buttonLeft = $('<button>').css({'margin-left':'30px'}).click(function(){clickButton(that.params['izquierda'])}).attr('id','Left').html('Left');
+  var buttonRight = $('<button>').css({'margin-left':'30px'}).click(function(){clickButton(that.params['derecha'])}).attr('id','Right').html('Right');
+  buttonPanel.append(buttonUp).append($('<br>')).append(buttonLeft).append($('<br>')).append(buttonRight).append($('<br>')).append(buttonDown);
   var video= $('<img src="http://' + window.location.host.split(":")[0] + ':8082/"/>')
         
-        
-  cheat_sheet.append(b).append(video)
-  /*for (var s in state){
-    cheat_sheet.append($('<li>').html(s+'<a class="type" onclick="loadHelp(\''+state[s].type+'\')" href="#">'+state[s].type+'</span>'))
-  }*/
-  
-  //tr.append($('<td style="vertical-align: top;">').append(code))
-  tr.append($('<td class="cheatsheet">').append("<h3>Movimientos ruedas</h3>").append(cheat_sheet)).attr('width: 500px;')
-  
-  $('#dialog #content').html(table)
- 	
-		var editor
-
-	
-	var editor=this.editor
-	$(editor).focus()
-	
-	$(editor).find('.CodeMirror-scroll').attr('height: 100%;')
-
- 
+  var contenido = $("<div>").append("<h3>Movimientos ruedas</h3>").append(buttonPanel).append(video)
+  $('#dialog #content').html(contenido)
 }
-// var botones_accept_configure = function(){
-//   this.params=this.getParams()
-//   this.update()
-// }
-
-//var botones_deactivate = function(){}
 
 var BotonesAction=extend(Action, {paramOptions:[
     {type:String,text:'arriba',name:'arriba'},
@@ -70,29 +41,9 @@ var BotonesAction=extend(Action, {paramOptions:[
     {type:String,text:'nombre',name:'nombre',show:true,default:"Botones"}
   ]})
 
-// BotonesAction.prototype.configure=function(){
-//   Node.prototype.configure.call(this);
-// }
-//BotonesAction.prototype.acceptConfigure=botones_accept_configure
 BotonesAction.prototype.activate=showInterface
-//BotonesAction.prototype.deactivate=botones_deactivate
-
-//BotonesAction.prototype.setName("HOLA");
-/*
-BotonesAction.prototype.paint=function(options){
-  Node.prototype.paint.call(this, options)
-  
-  $('#'+this.id+' text#param').text("HOLA")
-  //alert('#'+this.id+' text#param')
-  
-  return this.svggroup
-}*/
-
-
 
 main.behaviour.nodeFactory.add('botones',BotonesAction)
 
 })();
-function clickButton(id){
-  main.lua_exec('manager.notify('+id+')')
-}
+
